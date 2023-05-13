8ea0664c65201a9789fa3c93185833dfe8e82ce7
Maryna Shavlak <shavlakmaryna@gmail.com>
2023-05-13 23:08:15 +0300

    final code

--- scripts.js
+++ scripts.js
@@ -1,39 +1,18 @@
-/**1. Реалізація відкриття модального вікна із правилами гри
- * -реалізація кліку по кнопці "Правила"
- * - додавання класу modal--is-active  для блоку div.modal
- *
- *
- * 2. Реалізація закриття модального вікна із правилами гри
- * - видалення класу modal--is-active для блоку div.modal
- *
- *
- * 3. Фігура обирається:
- * - додавання класу gameContent--is-active  для блоку div.gameContent
- * - додавання класів при виборі різних фігур ГРАВЦЕМ. Якщо:
- *    1. Папір : gameContent__gameChoice gameContent__gameChoice--isPaper gameContent__gameChoice--isActive
- *    2. Камінь : gameContent__gameChoice gameContent__gameChoice--isRock gameContent__gameChoice--isActive
- *    3. Ножиці : gameContent__gameChoice gameContent__gameChoice--isScissors gameContent__gameChoice--isActive
- *
- * - додавання класів при виборі різних фігур КОМПОМ. :
- * -додати таймер
- * - після того, як час вийшов  обрати компом "випадкову фігуру"
- *    -  додати клас  gameContent__gameChoice gameContent__gameChoice--isComputer   (gameContent__gameChoice--isScissors || gameContent__gameChoice--isPaper || gameContent__gameChoice--isRock)
- *    -  для блоку із класом gameContent__gameChoiceImage прописати шлях до картинки (src)
- *
- * 4. Закінчення гри:
- * - додавання класу gameContent--is-active  gameContent--revealResult для блоку div.gameContent
- * - вивести текст із результатом гри у блоці gameContent__resultText (Draw, You win, You lose)
- * - обновити загальний бал header__scoreNumber
- *
- * 5. Перезапуск гри:
- * - видалення всіх додаткових класів для блоку div.gameContent
- * */
-
 const btnOpenModal = document.querySelector('.container__rules');
 const modalWindow = document.querySelector('.modal');
 const btnCloseModal = document.querySelector('.modal__closeIcon');
 const modalOverlay = document.querySelector('.modal__overlay');
 const gameContent = document.querySelector('.gameContent');
+const countdownText = document.querySelector('.gameContent__countdownText');
+const resultText = document.querySelector('.gameContent__resultText');
+const scoreBlockText = document.querySelector('.header__scoreNumber');
+const reloadBtn = document.querySelector('.gameContent__resultButton');
+const gameChoiceComputerImage = document.querySelector(
+  '.gameContent__gameChoiceImage',
+);
+const gameChoiceComputer = document.querySelector(
+  '.gameContent__gameChoice--isComputer',
+);
 const gameChoicePaper = document.querySelector(
   '.gameContent__gameChoice--isPaper',
 );
@@ -43,6 +22,10 @@ const gameChoiceRock = document.querySelector(
 const gameChoiceScissors = document.querySelector(
   '.gameContent__gameChoice--isScissors',
 );
+const choices = ['scissors', 'paper', 'rock'];
+let userChoice;
+let computerChoice;
+let gameScore = 0;
 
 btnOpenModal.onclick = toggleModal;
 btnCloseModal.onclick = toggleModal;
@@ -50,15 +33,101 @@ modalOverlay.onclick = toggleModal;
 gameChoicePaper.onclick = onChoiceClick;
 gameChoiceRock.onclick = onChoiceClick;
 gameChoiceScissors.onclick = onChoiceClick;
+reloadBtn.onclick = reload;
 
 function toggleModal() {
   modalWindow.classList.toggle('modal--isActive');
 }
 function startChoice() {
-  gameContent.classList.toggle('gameContent--isActive');
+  gameContent.classList.add('gameContent--isActive');
+  let timer = 3;
+  let timerId = setInterval(function () {
+    timer -= 1;
+    countdownText.innerHTML = timer;
+    if (!timer) {
+      countdownText.innerHTML = '';
+      clearInterval(timerId);
+      finish();
+    }
+  }, 500);
 }
 
 function onChoiceClick(e) {
-  console.log(e);
+  const selectedChoice = e.currentTarget;
+  selectedChoice.classList.add('gameContent__gameChoice--isActive');
   startChoice();
+  switch (e.currentTarget) {
+    case gameChoiceRock:
+      userChoice = choices[2];
+      break;
+    case gameChoicePaper:
+      userChoice = choices[1];
+      break;
+    default:
+      userChoice = choices[0];
+  }
+}
+
+function finish() {
+  randomComputerChoice();
+  gameContent.classList.add('gameContent--revealResult');
+  result();
+}
+
+function reload() {
+  gameContent.classList.remove(
+    'gameContent--isActive',
+    'gameContent--revealResult',
+  );
+  let activeElement = document.querySelector(
+    '.gameContent__gameChoice--isActive',
+  );
+  activeElement.classList.remove('gameContent__gameChoice--isActive');
+  gameChoiceComputerImage.src = '';
+  gameChoiceComputer.className =
+    'gameContent__gameChoice gameContent__gameChoice--isComputer';
+  countdownText.innerText = 3;
+}
+
+function randomComputerChoice() {
+  const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+  switch (randomChoice) {
+    case choices[2]:
+      computerChoice = choices[2];
+      break;
+    case choices[1]:
+      computerChoice = choices[1];
+      break;
+    default:
+      computerChoice = choices[0];
+  }
+  const capitalizedChoice =
+    randomChoice.charAt(0).toUpperCase() + randomChoice.slice(1);
+
+  gameChoiceComputer.classList.add(
+    `gameContent__gameChoice--is${capitalizedChoice}`,
+  );
+  gameChoiceComputerImage.src = `images/icon-${randomChoice}.svg`;
+}
+
+function result() {
+  if (userChoice === computerChoice) {
+    resultText.innerText = 'Draw';
+  } else if (
+    (userChoice == choices[1] && computerChoice == choices[1]) ||
+    (userChoice == choices[0] && computerChoice == choices[1]) ||
+    (userChoice == choices[2] && computerChoice == choices[0])
+  ) {
+    resultText.innerText = 'You win';
+    updateScore(1);
+  } else {
+    resultText.innerText = 'You lose';
+    updateScore(-1);
+  }
+}
+
+function updateScore(score) {
+  if (!gameScore && score < 0) return;
+  gameScore += score;
+  scoreBlockText.innerText = gameScore;
 }
